c8fe3fb25318c7066934aa8acedd7cbb18856243
sok213 <sophanarithsok@gmail.com>
2017-01-01 03:50:36 -0800

    update

--- dist/server.js
+++ dist/server.js
@@ -3,8 +3,24 @@
 var express = require('express'),
     ejs = require('ejs'),
     app = express(),
+    path = require('path'),
     http = require('http').Server(app),
-    port = process.env.PORT || 3000;
+    url = require('url'),
+    port = process.env.PORT || 3000,
+    routes = require('./routes/index');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//Pipe the css
+app.use('/', express.static('public'));
+//calls the routes javascript file.
+app.get('/', routes);
+
+function onRequest(req, res) {
+	console.log('Listening on port: ' + port);
+	var parsed = url.parse(req.url).href.split('/').join('');
+	console.log("parsed :" + parsed);
+}
 
 //lets require/import the mongodb native drivers.
 var mongodb = require('mongodb');
@@ -15,23 +31,27 @@ var MongoClient = mongodb.MongoClient;
 // Connection URL. This is where your mongodb server is running.
 
 //(Focus on This Variable)
-var url = process.env.MONGOLAB_URI;
+var mongoLabUrl = process.env.MONGOLAB_URI;
 //(Focus on This Variable)
 
 // Use connect method to connect to the Server
-MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
+MongoClient.connect(mongoLabUrl, function (err, db) {
+	if (err) {
+		console.log('Unable to connect to the mongoDB server. Error:', err);
+	} else {
+		console.log('Connection established to', mongoLabUrl);
 
-    // do some work here with the database.
+		// do some work here with the database.
 
-    //Close connection
-    db.close();
-  }
+		//Close connection
+		db.close();
+	}
 });
 
+//Create the http server.
+//http.createServer(onRequest).listen(port);
+//console.log('Server is now running on port: ' + port);
+
 http.listen(port, function () {
-  console.log('Listening on port: ' + port);
+	console.log('Listening on port: ' + port);
 });
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+	router = express.Router();
+
+router.get('/', function(req, res, next) {
+	res.render('index', {
+		title: 'Honey, I shrunk the URL!'
+	});
+});
+
+module.exports = router;
